93d5db13f3244e6396961f7139ef4b911a039fb3
Agung_Aditia <aditiaa55@gmail.com>
2017-01-26 23:32:18 +0700

    Perubahan Ketiga Tambah Luas Segitia

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -4,8 +4,11 @@ class HitungLuas2{
 		{
 			return panjang * lebar;
 		}
+	public static int LuasSegitiga(int alas, int tinggi)
+	{
+		return ((alas * tinggi)/2);
+	}
 	public static void main (String[] args)
 	{
 	}
 }
-
